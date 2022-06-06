0cec7e67fc0abd45c5e9d2d752778d20f45aea44
GhassanKY <106597775+GhassanKY@users.noreply.github.com>
2022-06-06 12:45:41 +0200

    Lista de la compra (Pantalla Principal)

---
+++ Funcionalidad.js
@@ -0,0 +1,66 @@
+// Informacion de las fechas/horas/dias/meses
+const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+
+// Contenedor de dia/semana/mes/año
+const tasksContainer = document.getElementById('tasksContainer');
+
+const setDate = () => {
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
+    dateText.textContent = date.toLocaleString('es', { weekday: 'long' });
+    dateMonth.textContent = date.toLocaleString('es', { month: 'short' });
+    dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
+};
+
+//Agregar una nueva tarea
+
+const addNewTask = event => { 
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+    
+    let cerrar = document.createElement("span");
+    cerrar.innerText = "x";
+    cerrar.classList.add ('close')
+    cerrar.onclick = function borrar (ev){
+        console.log(ev)
+        console.log(ev.target);
+        if (window.confirm("Se va a eliminar")){
+            ev.target.parentNode.remove();
+        }
+        
+    }
+    task.appendChild(cerrar)
+
+
+
+
+};
+
+const changeTaskState = event => {
+    event.target.classList.toggle('done');
+};
+
+const order = () => {
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach( el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el)
+    })
+    return [...toDo, ...done];
+}
+
+const renderOrderedTasks = () => {
+    order().forEach(el => tasksContainer.appendChild(el))
+}
+
+
